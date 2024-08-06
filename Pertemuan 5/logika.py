d32a77d523c9de52ed3551d615f8f7ae2183a3d9
azmi098 <azmijulvi91@gmail.com>
2024-08-06 09:43:18 +0700

    Add files via upload

---
+++ Pertemuan 5/logika.py
@@ -0,0 +1,16 @@
+x = True
+y = False
+
+print("and")
+print(x and y)
+print(x and x)
+print(y and y)
+
+print("or")
+print(x or y)
+print(x or x)
+print(y or y)
+
+print("not")
+print(not x)
+print(not y)
---
+++ Pertemuan 5/membership.py
@@ -0,0 +1,7 @@
+makanan = ["rujak", "bakso", "nasi kuning", "nasi"]
+
+print("apakah nasi uduk merupakan anggota", "nasi uduk" in makanan)
+print("apakah nasi kuning merupakan anggota", "nasi kuning" in makanan)
+
+print("apakah nasi uduk merupakan anggota", "nasi uduk" not in makanan)
+print("apakah nasi kuning merupakan anggota", "nasi kuning" not in makanan)
